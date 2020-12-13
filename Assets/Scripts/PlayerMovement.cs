aca9fbd6c6d7bb55ac443da7443fc063f878a93b
Epic Gamer <43441605+JoriKos@users.noreply.github.com>
2020-12-13 16:52:32 +0100

    Basic setup

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float MIN_X;
+    [SerializeField] private float MAX_X;
+    [SerializeField] private float MIN_Y;
+    [SerializeField] private float MAX_Y;
+    [SerializeField] private float MIN_Z;
+    [SerializeField] private float MAX_Z;
+    
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * speed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * speed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * speed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * speed);
+        }
+        transform.position = new Vector3(
+            Mathf.Clamp(transform.position.x, MIN_X, MAX_X),
+            Mathf.Clamp(transform.position.y, MIN_Y, MAX_Y),
+            Mathf.Clamp(transform.position.z, MIN_Z, MAX_Z));
+    }
+}
