176ad16018501f7cd5c709cede8da2a71a579303
Epic Gamer <43441605+JoriKos@users.noreply.github.com>
2020-12-16 22:43:31 +0100

    Decal system, shooting system, more targets

---
+++ Assets/Scripts/BulletPhysics.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletPhysics : MonoBehaviour
+{
+    private Shoot shoot;
+    private Rigidbody rb;
+    private Ray rayOrigin;
+    private Camera camera;
+    private RaycastHit hitInfo;
+    private float windSpeed;
+    [SerializeField] private float speed;
+    [SerializeField] private GameObject bulletHolePrefab;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        camera = FindObjectOfType<Camera>();
+        shoot = camera.GetComponent<Shoot>();
+        transform.position = camera.transform.position;
+        windSpeed = 0;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(new Vector3(-speed, 0 + (Physics.gravity.y * 0.001f), 0 + windSpeed), ForceMode.Force); //Object is given force, affected by gravity and windspeed.
+    }
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "TargetWall")
+        {
+            //If a target is hit, a raycast is used to determine the bullethole position, object is then destroyed
+            if (Physics.Raycast(new Vector3(rb.transform.position.x + 20, rb.transform.position.y, rb.transform.position.z), rb.transform.up, out hitInfo))
+            {
+                Instantiate(bulletHolePrefab, new Vector3(hitInfo.point.x + 0.19f, hitInfo.point.y, hitInfo.point.z), Quaternion.LookRotation(hitInfo.normal));
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    private Ray rayOrigin;
+    private Camera camera;
+    private Vector3 endGizmo;
+    private Vector3 startGizmo;
+    private RaycastHit hitInfo;
+    [SerializeField] private float gizmoLength;
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private GameObject bulletHolePrefab;
+
+
+    private void Start()
+    {
+        camera = FindObjectOfType<Camera>();
+        startGizmo = transform.position;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(bulletPrefab);
+            //Create hitscan bullet hole
+            if (Physics.Raycast(new Vector3(camera.transform.position.x, camera.transform.position.y, camera.transform.position.z - 7), camera.transform.forward, out hitInfo)) 
+            {
+                Instantiate(bulletHolePrefab, new Vector3(hitInfo.point.x + 0.19f, hitInfo.point.y, hitInfo.point.z), Quaternion.LookRotation(hitInfo.normal));
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        for (int i = 0; i < 20; i++) //Create hitscan aim gizmo
+        {
+            endGizmo = new Vector3(startGizmo.x - gizmoLength, startGizmo.y, startGizmo.z);
+            Gizmos.DrawLine(startGizmo, endGizmo);
+            startGizmo = transform.position;
+        }
+    }
+}
