1fb698c09059c832027d9cccdda8dacb67894e6c
Epic Gamer <43441605+JoriKos@users.noreply.github.com>
2021-01-20 10:33:21 +0100

    Wind indicator

---
+++ Assets/Scripts/Windicator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Windicator : MonoBehaviour
+{
+    private Quaternion rotation;
+    private Shoot shoot;
+
+    private void Awake()
+    {
+        shoot = FindObjectOfType<Camera>().GetComponent<Shoot>();
+    }
+
+    private void Update()
+    {
+        rotation = Quaternion.Euler(shoot.GetVectorWind());
+        this.gameObject.transform.rotation = rotation;
+    }
+}
